bc7e8e0c2b60bb14694e39e435c9d0b6b9fc1bfe
uncle-brother-son <wayne@unclebrotherson.com>
2026-02-09 23:20:17 +0000

    Remove invalid deployment config from sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -4,12 +4,5 @@ export default defineCliConfig({
   api: {
     projectId: 'ddua7oi6',
     dataset: 'production'
-  },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
   }
 })
