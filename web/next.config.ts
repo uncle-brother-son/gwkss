bd29c584316b0d697680f1b906b1eb4ff68de76d
uncle-brother-son <wayne@unclebrotherson.com>
2026-03-06 23:49:23 +0000

    Same as last time

--- web/next.config.ts
+++ web/next.config.ts
@@ -11,4 +11,6 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+if (process.env.NODE_ENV === 'development') {
+  import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+}
