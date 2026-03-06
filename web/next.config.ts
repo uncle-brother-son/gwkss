67d8e2bc4578c23b8b4d38129ad5dd82bc42fdc7
uncle-brother-son <wayne@unclebrotherson.com>
2026-03-06 23:47:02 +0000

    Same as last time

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,9 +4,11 @@ const nextConfig: NextConfig = {
   experimental: {
     webpackBuildWorker: true,
   },
+  images: {
+    unoptimized: true, // Cloudflare doesn't support Next.js Image Optimization API
+  },
 };
 
 export default nextConfig;
 
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
