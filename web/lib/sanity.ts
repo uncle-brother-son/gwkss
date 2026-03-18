9baf0ad17d547c9f2fe61b70954b62054d81b72c
uncle-brother-son <wayne@unclebrotherson.com>
2026-03-18 11:54:21 +0000

    Content not updating fix - useCdn: false

--- web/lib/sanity.ts
+++ web/lib/sanity.ts
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false, // Disable CDN for instant updates via webhook
 });
 
 const builder = createImageUrlBuilder(client);
